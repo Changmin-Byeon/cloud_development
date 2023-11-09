ddceccb592f192cf3b429df7d1287762398e4b4f
c00302990 <c00302990@setu.ie>
2023-11-10 02:34:15 +0900

    Third version of webapp deployed to PythonAnywhere.

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request, redirect  # from module import Class.
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    session,
+)  # from module import Class.
 
 import os
 import re
@@ -6,14 +12,19 @@ import swim_utils
 import hfpy_utils
 
 app = Flask(__name__)
+app.secret_key = "cloud development ca"
 
 files = os.listdir(swim_utils.FOLDER)
 files.remove(".DS_Store")
 names = set()
-name_dict = {}
+name_age = {}
+show_events = {}
 
 for swimmer in files:
-        names.add(swim_utils.get_swimmers_data(swimmer)[0])
+    names.add(swim_utils.get_swimmers_data(swimmer)[0])
+    name_age[swim_utils.get_swimmers_data(swimmer)[0]] = swim_utils.get_swimmers_data(
+        swimmer
+    )[1]
 
 
 @app.get("/")
@@ -21,12 +32,25 @@ def hello():
     return "Hello from my first web app - cool, isn't it?"  # ANY string.
 
 
-@app.route('/redirect', methods=['GET','POST'])
+@app.route("/redirect", methods=["GET", "POST"])
 def back_to_select_swimmer():
+    session.pop("swimmer", None)
     return redirect("/getswimmers")
+
 
 @app.post("/chart")
 def display_chart():
+    filename = (
+        session["swimmer"]
+        + "-"
+        + name_age[session["swimmer"]]
+        + "-"
+        + request.form["event"].split(" ")[0]
+        + "-"
+        + request.form["event"].split(" ")[1]
+        + ".txt"
+    )
+
     (
         name,
         age,
@@ -35,13 +59,13 @@ def display_chart():
         the_times,
         converts,
         the_average,
-    ) = swim_utils.get_swimmers_data(request.form["event"])
+    ) = swim_utils.get_swimmers_data(filename)
 
     the_title = f"{name} (Under {age}) {distance} - {stroke}"
     from_max = max(converts) + 50
     the_converts = [hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts]
 
-    the_data = zip(the_converts, the_times)
+    the_data = zip(reversed(the_converts), reversed(the_times))
 
     return render_template(
         "chart.html",
@@ -55,27 +79,28 @@ def display_chart():
 
 @app.get("/getswimmers")
 def get_swimmers_name():
-
     return render_template(
         "select_swimmer.html",
         title="Select a swimmer to chart",
         data=sorted(names),
+        data2=name_age,
     )
 
 
 @app.post("/displayevents")
 def get_swimmer_events():
-    file_list = []
-    for event in files:
-        if re.match(request.form["swimmer"], event):
-            file_list.append(event)
-            name_dict[request.form["swimmer"]] = file_list
+    session["swimmer"] = request.form["swimmer"]
+    event_list = []
+    for file in files:
+        if re.match(request.form["swimmer"], file):
+            event = file.split("-")[2] + " " + file.split("-")[3].removesuffix(".txt")
+            event_list.append(event)
+            show_events[request.form["swimmer"]] = event_list
 
-        
     return render_template(
         "select_event.html",
         title="Select a event to chart",
-        files=sorted(name_dict[request.form["swimmer"]]),
+        files=sorted(show_events[request.form["swimmer"]]),
     )
 
 
